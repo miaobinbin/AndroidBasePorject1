939b0bfe8e65a6d626b0769420e85923b53bcaeb
miaobinbin <miaobinbin@tuniu.com>
2016-10-21 09:50:58 +0800

    newproject

---
+++ YuyuMall_Android/app/src/androidTest/java/com/yuyuto/mall/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.yuyuto.mall;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ *  项目规范参照 http://www.jianshu.com/p/d9e4ddd1c530
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
